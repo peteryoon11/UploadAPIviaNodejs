2ef06c3b5adf4ff958849cf08c9742e7e54f12c3
jisub.yoon <yjisub11@gmail.com>
2018-01-21 19:50:36 +0900

    readme file 추가 및 architecture 몇개

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # UploadAPIviaNodejs
 nodejs를 통한 file upload
-
-**필요한 npm
+version info 
+v1.0.0
+- - -
+> 필요한 npm
 
 1. ftp
 2.fs
+
+- - -
+### 분류
+>1. server 
+  1) put , post 방식으로 binary file 업로드
+  2) 중간에 인증 하는 방식에 대해서는 고민중 
+  3) 윈도우에서 테스트 하는 거니까 윈도우 iis ftp server 를 사용해 볼까 
+>2. client 
+  1) java를 이용해서 업로드 하는 방식과 
+  2) node를 이용해서 url을 만든후에 만드는 방식을 고려 중에 있음 
+
+### 실습 목표
+1. 간략하게
+2. 모듈은 최대한 나누기
+3. 평소에 안썼지만 nodejs 에서 트렌드로 쓰는 부분 사용해 보기 
+4. test code 부분 적용 해보기 
--- server/upload_app.js
+++ server/upload_app.js
@@ -1,16 +1,16 @@
-var cluster = require("cluster");
+//var cluster = require("cluster");
 var os = require("os");
-var request = require('request');
+//var request = require('request');
 var fs = require('fs');
 var express = require('express');
-var multipart = require('connect-multiparty');
-var multipartMiddleware = multipart();
+//var multipart = require('connect-multiparty');
+//var multipartMiddleware = multipart();
 var app = express();
 var mysql = require('mysql');
 var http = require('http');
-var uuid = require('node-uuid');
+//var uuid = require('node-uuid');
 
-var sh = require('shelljs/global');
+/* var sh = require('shelljs/global');
 var https = require('https');
 
 var net = require('net');
@@ -20,7 +20,7 @@ var dateformat = require('dateformat');
 var pathmodule = require('path');
 
 var rimraf = require('rimraf');
-var HashMap = require('hashmap');
+var HashMap = require('hashmap'); */
 
 
 var config = require('./config.json');
@@ -31,8 +31,19 @@ var config = require('./config.json');
 
 app.post("/upload",Upload);
 app.put("/upload", Upload);
+app.listen(config.server.server_port, function () {
+    console.log('Example app listening on port ' + config.server.server_port);
 
-function Upload()
+    
+});
+function Upload(req,res)
 {
+
+    
+    if ('POST' == req.method) {
+        console.log("call by post");      
+    } else if ('PUT' == req.method) {
+        console.log("call by put");      
+    }
 
 }
